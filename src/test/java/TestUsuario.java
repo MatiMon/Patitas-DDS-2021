39fea2b26445dbcfc5c5e0db335e55c4881e60e4
mati <matimon@frba.utn.edu.ar>
2021-05-06 23:06:20 -0300

    Ultimos test de usuario

--- src/test/java/TestUsuario.java
+++ src/test/java/TestUsuario.java
@@ -29,4 +29,27 @@ public class TestUsuario {
     Usuario usuario = new Usuario("riquelme", "c0ntr4s3n14c0nmuchaseguridad!!?", validador);
     Assertions.assertFalse(usuario.autorizarContrasenia("soyunhackermalvado"));
   }
+
+  @Test
+  public void actualizarPasswordEIntentarEntrarConLaNueva() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    List<ValidacionDeContrasenia> validaciones = new ArrayList<>();
+    ValidacionDeLongitud validacionDeLongitud = new ValidacionDeLongitud();
+    validaciones.add(validacionDeLongitud);
+    ValidadorContrasenia validador = new ValidadorContrasenia(validaciones);
+    Usuario usuario = new Usuario("riquelme", "c0ntr4s3n14c0nmuchaseguridad!!?", validador);
+    usuario.actualizarContrasenia("daledaledaledalebo1234$$");
+    Assertions.assertTrue(usuario.autorizarContrasenia("daledaledaledalebo1234$$"));
+  }
+
+  @Test
+  public void actualizarPasswordEIntentarEntrarConLaVieja() throws NoSuchAlgorithmException, InvalidKeySpecException {
+    List<ValidacionDeContrasenia> validaciones = new ArrayList<>();
+    ValidacionDeLongitud validacionDeLongitud = new ValidacionDeLongitud();
+    validaciones.add(validacionDeLongitud);
+    ValidadorContrasenia validador = new ValidadorContrasenia(validaciones);
+    Usuario usuario = new Usuario("riquelme", "c0ntr4s3n14c0nmuchaseguridad!!?", validador);
+    usuario.actualizarContrasenia("daledaledaledalebo1234$$");
+    Assertions.assertFalse(usuario.autorizarContrasenia("c0ntr4s3n14c0nmuchaseguridad!!?"));
+  }
+
 }
