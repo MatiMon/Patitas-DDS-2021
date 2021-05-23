77f5961397ce33fd53525becc3345782d1c61311
Diana Lopez Alvas <dlopezalvas@gmail.com>
2021-05-23 20:42:32 -0300

    fix lista de opciones en clase caracPosibles

--- src/main/java/caracteristicas/CaracteristicaPosible.java
+++ src/main/java/caracteristicas/CaracteristicaPosible.java
@@ -7,16 +7,11 @@ public class CaracteristicaPosible {
     String nombre;
     Boolean esObligatoria;
     TipoCaracteristica tipo;
-    List<String> opciones = new ArrayList<>();
 
     public CaracteristicaPosible(String nombre, Boolean esObligatoria, TipoCaracteristica tipo) {
         this.nombre = nombre;
         this.esObligatoria = esObligatoria;
         this.tipo = tipo;
-    }
-
-    public void agregarOpcion(String opcion){
-        opciones.add(opcion);
     }
 
     public String getNombre() {
