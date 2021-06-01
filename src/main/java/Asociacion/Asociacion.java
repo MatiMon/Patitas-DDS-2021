87e7a51c73f73ddc205265f41e7a08795588f0b2
Lautaro <lauti.requejo@gmail.com>
2021-06-01 20:21:41 -0300

    agrego asociacion

---
+++ src/main/java/Asociacion/Asociacion.java
@@ -0,0 +1,28 @@
+package Asociacion;
+
+import java.util.List;
+
+public class Asociacion {
+  /*
+  private String nombre;
+  Ubicacion ubicacion;
+  List<RescateDeMascotaSinRegistrar> rescateDeMascotasSinRegistrar;
+  List<RescateDeMascotaRegistrada> rescateDeMascotasRegistradas;
+
+  public Asociacion(String nombre, Ubicacion ubicacion,
+                    List<RescateDeMascotaSinRegistrar> rescateDeMascotasSinRegistrar,
+                    List<RescateDeMascotaRegistrada> rescateDeMascotasRegistradas) {
+    this.nombre = nombre;
+    this.ubicacion = ubicacion;
+    this.rescateDeMascotasSinRegistrar = rescateDeMascotasSinRegistrar;
+    this.rescateDeMascotasRegistradas = rescateDeMascotasRegistradas;
+  }
+  public List<RescateDeMascotaSinRegistrar> obtenerPublicacionesSinAprobar() {
+    //TODO filtrar
+    return this.rescateDeMascotasSinRegistrar;
+  }
+  public List<RescateDeMascotaRegistrada> ultimasMascotasEncontradas(int dias) {
+    //TODO traer metodo de la clase RepoRescates y borrar la clase
+    return this.rescateDeMascotasRegistradas;
+  }*/
+}
