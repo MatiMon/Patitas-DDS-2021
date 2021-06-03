03de0eb52c83369735bfae71cb9d61be82fca52f
LucasUsh <lucaas-e@hotmail.com>
2021-06-02 22:06:42 -0300

    Agregar código QR a RescateDeMascotaRegistrada

--- src/main/java/rescate/RescateDeMascotaRegistrada.java
+++ src/main/java/rescate/RescateDeMascotaRegistrada.java
@@ -6,7 +6,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class RescateDeMascotaRegistrada extends RescateDeMascota {
-  public RescateDeMascotaRegistrada(List<String> fotos, String descripcion, String ubicacion, Rescatista rescatista, LocalDateTime fecha, Mascota mascota) {
+  String codigoQR;
+  
+  public RescateDeMascotaRegistrada(List<String> fotos, String descripcion, String ubicacion, Rescatista rescatista, LocalDateTime fecha, Mascota mascota, String QR) {
     super(fotos, descripcion, ubicacion, rescatista, fecha, mascota);
+    this.codigoQR = QR;
   }
 }
