76e93819d3d72888f8eb07750caef77f077e1a8a
LucasUsh <lucaas-e@hotmail.com>
2021-06-03 00:20:25 -0300

    Agregar estadoDeAprobacion, duenioSinRegistrar y numeroDePublicacion a RescateDeMascotaSinRegistrar

--- src/main/java/rescate/RescateDeMascotaRegistrada.java
+++ src/main/java/rescate/RescateDeMascotaRegistrada.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class RescateDeMascotaRegistrada extends RescateDeMascota {
-  String codigoQR;
+  private String codigoQR;
   
   public RescateDeMascotaRegistrada(List<String> fotos, String descripcion, String ubicacion, InvolucradoEnRescate rescatista, LocalDateTime fecha, Mascota mascota, String QR) {
     super(fotos, descripcion, ubicacion, rescatista, fecha, mascota);
--- src/main/java/rescate/RescateDeMascotaSinRegistrar.java
+++ src/main/java/rescate/RescateDeMascotaSinRegistrar.java
@@ -6,7 +6,34 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class RescateDeMascotaSinRegistrar extends RescateDeMascota {
-  public RescateDeMascotaSinRegistrar(List<String> fotos, String descripcion, String ubicacion, InvolucradoEnRescate rescatista, LocalDateTime fecha, Mascota mascota) {
+  private boolean estadoDeAprobacion;
+  private int numeroDePublicacion;
+  private InvolucradoEnRescate duenioSinRegistrar;
+
+  public RescateDeMascotaSinRegistrar(List<String> fotos, String descripcion, String ubicacion,
+                                      InvolucradoEnRescate rescatista, LocalDateTime fecha, Mascota mascota, int numeroDePublicacion) {
     super(fotos, descripcion, ubicacion, rescatista, fecha, mascota);
+    this.estadoDeAprobacion = false;
+    this.numeroDePublicacion = numeroDePublicacion;
+  }
+
+  public void aprobarPublicacion(boolean estadoDeAprobacion) {
+    this.estadoDeAprobacion = estadoDeAprobacion;
+  }
+
+  public boolean getEstadoDeAprobacion() {
+    return estadoDeAprobacion;
+  }
+
+  public int getNumeroDePublicacion() {
+    return numeroDePublicacion;
+  }
+
+  public InvolucradoEnRescate getDuenioSinRegistrar() {
+    return duenioSinRegistrar;
+  }
+
+  public void setDuenioSinRegistrar(InvolucradoEnRescate duenioSinRegistrar) {
+    this.duenioSinRegistrar = duenioSinRegistrar;
   }
 }
