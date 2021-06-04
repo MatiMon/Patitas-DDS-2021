e5308de3ce304447712356c22f6c285e5e5dac80
mati <matimon@frba.utn.edu.ar>
2021-06-04 19:18:17 -0300

    corrijo test

--- src/test/java/TestRepositorioQR.java
+++ src/test/java/TestRepositorioQR.java
@@ -22,52 +22,42 @@ public class TestRepositorioQR {
   private List<CaracteristicaDefinida> caracteristicas = new ArrayList<>();
   private CaracteristicaIdeal caracteristica = new CaracteristicaIdeal("Edad", true, new NumericaIdeal());
   private LocalDate fecha = LocalDate.parse("2018-10-30");
+  Duenio unDuenio = new Duenio("Juan",
+      "Perez",
+      fecha,
+      TipoDocumento.DNI,
+      999999,
+      contactos);
+  Mascota mascota1 = new Mascota("Toto", "Toto", 10,
+      Sexo.MACHO, TipoAnimal.PERRO, "perro grande", fotos,
+      caracteristicas, unDuenio, "qr-10", Tamanio.GRANDE
+  );
 
 
   @BeforeEach
-  void iniciarRepositorio(){
+  void iniciarRepositorio() {
     CaracteristicaDefinida caracteristicaDefinida = caracteristica.crearCaracteristica(10);
     caracteristicas.add(caracteristicaDefinida);
     contactos.add(contacto1);
     fotos.add("foto1");
-    Duenio unDuenio = new Duenio("Juan",
-        "Perez",
-        fecha,
-        TipoDocumento.DNI,
-        999999,
-        contactos);
-    Mascota mascota1 = new Mascota("Toto","Toto",10,
-        Sexo.MACHO, TipoAnimal.PERRO,"perro grande",fotos,
-        caracteristicas,unDuenio,"qr-10", Tamanio.GRANDE
-        );
 
-    repositorioDeQRs.getInstancia().registrarQR("qr-10",mascota1);
+    repositorioDeQRs.getInstancia().registrarQR("qr-10", mascota1);
   }
 
   @AfterEach
-  void vaciarRepositorio(){
+  void vaciarRepositorio() {
     repositorioDeQRs.getInstancia().removerQR("qr-10");
   }
 
   @Test
-  void sePuedeObtenerLaMascotaATravezDelQR(){
+  void sePuedeObtenerLaMascotaATravesDelQR() {
     CaracteristicaDefinida caracteristicaDefinida = caracteristica.crearCaracteristica(10);
     caracteristicas.add(caracteristicaDefinida);
     contactos.add(contacto1);
     fotos.add("foto1");
-    Duenio unDuenio = new Duenio("Juan",
-        "Perez",
-        fecha,
-        TipoDocumento.DNI,
-        999999,
-        contactos);
-    Mascota mascota1 = new Mascota("Toto","Toto",10,
-        Sexo.MACHO, TipoAnimal.PERRO,"perro grande",fotos,
-        caracteristicas,unDuenio,"qr-10", Tamanio.GRANDE
-    );
 
     Mascota mascotaSegunQR = repositorioDeQRs.getInstancia().obtenerMascota("qr-10");
-    Assertions.assertEquals(mascota1,mascotaSegunQR);
+    Assertions.assertEquals(mascota1, mascotaSegunQR);
   }
 
 }
