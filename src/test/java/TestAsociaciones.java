87d264240c899962f21db919c1f36cc1f6f29f73
Lautaro <lauti.requejo@gmail.com>
2021-06-03 23:53:41 -0300

    dependecias automaticas mvn

--- src/test/java/TestAsociaciones.java
+++ src/test/java/TestAsociaciones.java
@@ -1,7 +1,6 @@
 import Asociacion.Asociacion;
 import duenio.Contacto;
 import duenio.TipoDocumento;
-import mascota.Mascota;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
