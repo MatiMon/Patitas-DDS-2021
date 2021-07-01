87275df7a53c26c4867414cad13a5f19c4eda5d4
Gaston Gonzalez Sapia <ggonzalezsapia@gmail.com>
2021-07-01 01:32:47 -0300

    se agrega clase main con metodo de generarRecomendaciones comentado

---
+++ src/main/java/rescateDePatitasAPP/RescateDePatitasAPP.java
@@ -0,0 +1,18 @@
+package rescateDePatitasAPP;
+
+import mascota.RecomendadorMascotas;
+
+public class RescateDePatitasAPP {
+
+  public static void  main(String[] args){
+
+    RecomendadorMascotas recomendadorMascotas = new RecomendadorMascotas();
+
+    System.out.println("Ejemplo");
+
+    //TODO: completar plugin y probar en vm despues
+
+    //recomendadorMascotas.generarRecomendaciones();
+  }
+
+}
