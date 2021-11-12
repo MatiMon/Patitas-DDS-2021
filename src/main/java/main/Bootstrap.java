eb83160409e40c9b8a76e1106fcaf95f8101bf70
Diana Lopez Alvas <dlopezalvas@gmail.com>
2021-11-12 19:54:58 -0300

    crear admin en bootstrap

--- src/main/java/main/Bootstrap.java
+++ src/main/java/main/Bootstrap.java
@@ -2,6 +2,9 @@ package main;
 
 import com.sun.xml.internal.ws.developer.MemberSubmissionAddressing;
 import model.asociacion.Asociacion;
+import model.contacto.Contacto;
+import model.duenio.Duenio;
+import model.duenio.TipoDocumento;
 import model.mascota.Mascota;
 import model.mascota.Sexo;
 import model.mascota.Tamanio;
@@ -17,6 +20,7 @@ import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,15 +38,41 @@ public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, Tra
   public void run() {
 
 
-    Asociacion patitasAlRescate = new Asociacion("Patitas al rescate", new Ubicacion("String", 123, 456),
+/*    Asociacion patitasAlRescate = new Asociacion("Patitas al rescate", new Ubicacion("String", 123, 456),
         new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
     entityManager().persist(patitasAlRescate);
 
 
     Mascota mascota = new Mascota("Pepita", "Pepin", 15, Sexo.HEMBRA, TipoAnimal.GATO,
         null, new ArrayList<>(), new ArrayList<>(), null, "123", Tamanio.GRANDE);
-    entityManager().persist(mascota);
+    entityManager().persist(mascota);*/
 
+    //admin
+    persistAdmin("admin", "admin123456");
   }
+
+  public void persistAdmin(String user, String pass){
+    ValidacionDeCaracteresConsecutivos validacionDeCaracteresConsecutivos = new ValidacionDeCaracteresConsecutivos();
+    List<ValidacionDeContrasenia> validaciones = new ArrayList<>();
+    validaciones.add(validacionDeCaracteresConsecutivos);
+    ValidadorContrasenia validadorContrasenia = new ValidadorContrasenia(validaciones);
+    Usuario userAdmin = null;
+    try{
+      userAdmin = new Usuario(user, pass, validadorContrasenia);
+    }catch(Exception e){
+      //...
+    }
+
+    userAdmin.setEsAdministrador(true);
+
+    Contacto contactoPrincipal = new Contacto("al", "go", 1234, "algo@gmail");
+    Ubicacion ubicacion = new Ubicacion("String", 123, 456);
+    Duenio duenio = new Duenio("Nombre falso", "sanchez", LocalDate.now(), TipoDocumento.DNI, 1254589, contactoPrincipal, ubicacion, userAdmin);
+
+    entityManager().persist(userAdmin);
+    entityManager().persist(contactoPrincipal);
+    entityManager().persist(duenio);
+  }
+
 
 }
